6ff422c2915c2d3628673eff58820cb3330a920e
Heya <heya@Heyas-MacBook-Air.local>
2020-09-09 17:17:37 +0300

    Updates

---
+++ precache-manifest.b96b49fce83a56c0e3f09e72de555a74.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92505f09de4f209ff3af541006fdead5",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "74f1b393570800aad861",
+    "url": "/tic-tac-toe/static/css/main.11ebf045.chunk.css"
+  },
+  {
+    "revision": "7de1f036a2157dac2058",
+    "url": "/tic-tac-toe/static/js/2.57343eaf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tic-tac-toe/static/js/2.57343eaf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74f1b393570800aad861",
+    "url": "/tic-tac-toe/static/js/main.4be725ea.chunk.js"
+  },
+  {
+    "revision": "7f32745e7b69030faee7",
+    "url": "/tic-tac-toe/static/js/runtime-main.df28867d.js"
+  }
+]);
